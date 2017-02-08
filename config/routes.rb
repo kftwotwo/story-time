0faf48d877c9a0761d325bbe8334cc9d0ba34283
Diego Suarez and Kevin Finley <diego+kevin@>
2017-02-08 09:48:20 -0800

    add authenication for story time site

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  get 'signup'  => 'users#new'
+  resources :users
+
+   get 'login' => 'sessions#new'
+   post 'login' => 'sessions#create'
+   root 'sessions#new' 
+   delete 'logout' => 'sessions#destroy'
+
+end
